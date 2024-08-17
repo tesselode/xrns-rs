55ad2809b4048ec38fb662cd1f87997ae08d2653
Andrew Minnich <aminnich3@gmail.com>
2024-08-17 17:08:46 -0400

    tabs

--- src/lib.rs
+++ src/lib.rs
@@ -5,100 +5,100 @@ use serde::{Deserialize, Deserializer};
 use zip::{result::ZipError, ZipArchive};
 
 pub fn load(path: impl AsRef<Path>) -> Result<Song, Error> {
-    let path = path.as_ref();
-    let file = File::open(path)?;
-    let mut archive = ZipArchive::new(file)?;
-    let mut song_xml_file = archive.by_name("Song.xml")?;
-    let mut song_xml_string = String::new();
-    song_xml_file.read_to_string(&mut song_xml_string)?;
-    let song = quick_xml::de::from_str(&song_xml_string)?;
-    Ok(song)
+	let path = path.as_ref();
+	let file = File::open(path)?;
+	let mut archive = ZipArchive::new(file)?;
+	let mut song_xml_file = archive.by_name("Song.xml")?;
+	let mut song_xml_string = String::new();
+	song_xml_file.read_to_string(&mut song_xml_string)?;
+	let song = quick_xml::de::from_str(&song_xml_string)?;
+	Ok(song)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Song {
-    pub global_song_data: GlobalSongData,
-    pub pattern_pool: PatternPool,
+	pub global_song_data: GlobalSongData,
+	pub pattern_pool: PatternPool,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct GlobalSongData {
-    pub beats_per_min: u32,
-    pub lines_per_beat: u32,
-    pub ticks_per_line: u32,
-    pub signature_numerator: u32,
-    pub signature_denominator: u32,
+	pub beats_per_min: u32,
+	pub lines_per_beat: u32,
+	pub ticks_per_line: u32,
+	pub signature_numerator: u32,
+	pub signature_denominator: u32,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct PatternPool {
-    #[serde(deserialize_with = "unwrap_pattern_list")]
-    pub patterns: Vec<Pattern>,
+	#[serde(deserialize_with = "unwrap_pattern_list")]
+	pub patterns: Vec<Pattern>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Pattern {
-    pub number_of_lines: u32,
-    pub tracks: Tracks,
+	pub number_of_lines: u32,
+	pub tracks: Tracks,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Tracks {
-    #[serde(rename = "PatternTrack")]
-    pub pattern_tracks: Vec<PatternTrack>,
+	#[serde(rename = "PatternTrack")]
+	pub pattern_tracks: Vec<PatternTrack>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct PatternTrack {
-    #[serde(deserialize_with = "unwrap_line_list")]
-    pub lines: Vec<Line>,
+	#[serde(deserialize_with = "unwrap_line_list")]
+	pub lines: Vec<Line>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Line {
-    #[serde(rename = "@index")]
-    pub index: u32,
-    #[serde(deserialize_with = "unwrap_note_column_list")]
-    pub note_columns: Vec<NoteColumn>,
+	#[serde(rename = "@index")]
+	pub index: u32,
+	#[serde(deserialize_with = "unwrap_note_column_list")]
+	pub note_columns: Vec<NoteColumn>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct NoteColumn {
-    pub note: String,
-    pub instrument: Option<String>,
-    pub volume: Option<String>,
-    pub panning: Option<String>,
-    pub delay: Option<String>,
-    pub effect_number: Option<String>,
-    pub effect_value: Option<String>,
+	pub note: String,
+	pub instrument: Option<String>,
+	pub volume: Option<String>,
+	pub panning: Option<String>,
+	pub delay: Option<String>,
+	pub effect_number: Option<String>,
+	pub effect_value: Option<String>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct PatternSequence {
-    #[serde(deserialize_with = "unwrap_sequence_entry_list")]
-    pub sequence_entries: Vec<SequenceEntry>,
+	#[serde(deserialize_with = "unwrap_sequence_entry_list")]
+	pub sequence_entries: Vec<SequenceEntry>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct SequenceEntry {
-    pub pattern: usize,
+	pub pattern: usize,
 }
 
 #[derive(Debug, From, Error, Display)]
 pub enum Error {
-    IoError(std::io::Error),
-    ZipError(ZipError),
-    DeError(quick_xml::DeError),
+	IoError(std::io::Error),
+	ZipError(ZipError),
+	DeError(quick_xml::DeError),
 }
 
 macro_rules! unwrap_list_fns {
